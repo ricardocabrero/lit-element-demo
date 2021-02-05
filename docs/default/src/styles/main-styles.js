e7d23898152c6f72c5bd09df825e6bbf0efcd5a2
Cabrero Gutiérrez <rocz@espc013321.gft.com>
2021-02-05 14:44:50 +0100

    updated

--- docs/default/src/styles/main-styles.js
+++
@@ -1,7 +0,0 @@
-import { css } from "../../node_modules/lit-element/lit-element.js";
-export const styles = css`
-    :host {
-        display: block;
-        height: 100vh;
-        background: #f9f9f9;
-    }`;
--- docs/default/src/utils/dataFetch.js
+++
@@ -1,7 +0,0 @@
-const fetchData = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-  return data;
-};
-
-export default fetchData;
